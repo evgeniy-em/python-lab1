ee592abf2681f2ac12734d659e3b3923e319a6c6
evgeniyem <dossska.ru@gmail.com>
2023-08-13 10:16:58 +0700

    Add template

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,11 +1,19 @@
-print("Hello world")
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return "<html><head></head><body>Hello world!</body></html>"
+
+
+@app.route("/data_to")
+def data_to():
+    some_pars = {'user': 'Ivan', 'color': 'red'}
+    some_str = 'Hello my dear friends'
+    some_value = 10
+    return render_template('simple.html', some_str=some_str, some_value=some_value, some_pars=some_pars)
 
 
 if __name__ == "__main__":
